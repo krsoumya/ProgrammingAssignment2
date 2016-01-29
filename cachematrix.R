dd7ca93ca9360dab7595661f816839c66eb76959
krsoumya <ksoumya.rajagopalan@gmail.com>
2016-01-29 23:46:15 +0530

    Submission for Programming Assignment
    The functions are used to calculate the inverse of a 'special' matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function is to create the matrix, obtain the values of the matrix and to obtain the value of the inverse of the matrix created. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    make <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    receive <- function() x
+    makeinverse <- function(inv) i <<- inv
+    receiveinverse <- function() i
+    list(
+        make = make,
+        receive = receive,
+        makeinverse = makeinverse,
+        receiveinverse = receiveinverse
+    )
 }
 
-
-## Write a short comment describing this function
+## The function calculates the inverse of the matrix and returns the value of that matrix. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$receiveinverse()
+    if(!is.null(i)) {
+        message("Receiving the cached data")
+        return(i)
+    }
+    m <- x$receive()
+    i <- solve(m, ...)
+    x$makeinverse(i)
+    i
 }
